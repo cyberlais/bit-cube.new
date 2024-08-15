ea5418a32c1ed7e0d0cbc6aaa093b80b59e812eb
Alexandr <mr.lais49@gmail.com>
2024-08-15 23:18:19 +0300

    обновил данные в моделях

--- src/models.ts
+++ src/models.ts
@@ -11,47 +11,47 @@ export const minerModels: MinerModel[] = [
     label: "WHATS MINER M50",
     model: "M50",
     hashrate: 118,
-    powerConsumption: 29,
-    asicPrice: 3422,
+    powerConsumption: 3422,
+    asicPrice: 1150,
     image: "./img/calculator/models/M50.jpg"
   },
   {
     label: "WHATS MINER M60",
     model: "M60",
     hashrate: 172,
-    powerConsumption: 19.9,
-    asicPrice: 3423,
+    powerConsumption: 3423,
+    asicPrice: 3130,
     image: "./img/calculator/models/M60.jpg"
   },
   {
     label: "WHATS MINER M60",
     model: "M60",
     hashrate: 170,
-    powerConsumption: 19.9,
-    asicPrice: 3383,
+    powerConsumption: 3383,
+    asicPrice: 3090,
     image: "./img/calculator/models/M60.jpg"
   },
   {
     label: "WHATS MINER M50S++",
     model: "M50S++",
     hashrate: 140,
-    powerConsumption: 22,
-    asicPrice: 3080,
+    powerConsumption: 3080,
+    asicPrice: 2280,
     image: "./img/calculator/models/M50S++.jpg"
   },
   {
     label: "WHATS MINER M56S++",
     model: "M56S++",
     hashrate: 242,
-    powerConsumption: 22.5,
-    asicPrice: 5324,
+    powerConsumption: 5324,
+    asicPrice: 4200,
     image: "./img/calculator/models/M56S++.jpg"
   },
   {
     label: "WHATS MINER M66",
     model: "M66",
     hashrate: 256,
-    powerConsumption: 19.9,
+    powerConsumption: 5094,
     asicPrice: 4650,
     image: "./img/calculator/models/M66.jpg"
   },
@@ -59,55 +59,55 @@ export const minerModels: MinerModel[] = [
     label: "WHATS MINER M66",
     model: "M66",
     hashrate: 282,
-    powerConsumption: 19.9,
-    asicPrice: 5612,
+    powerConsumption: 5612,
+    asicPrice: 5200,
     image: "./img/calculator/models/M66.jpg"
   },
   {
     label: "ANTMINER S19 SERIES",
     model: "S19k Pro",
     hashrate: 120,
-    powerConsumption: 23,
-    asicPrice: 2760,
+    powerConsumption: 2760,
+    asicPrice: 1050,
     image: "./img/calculator/models/S19k Pro.jpg"
   },
   {
     label: "ANTMINER S19 SERIES",
     model: "S19k Pro",
     hashrate: 115,
-    powerConsumption: 23,
-    asicPrice: 2645,
+    powerConsumption: 2645,
+    asicPrice: 980,
     image: "./img/calculator/models/S19k Pro.jpg"
   },
   {
     label: "ANTMINER S19 SERIES",
     model: "S19k Pro",
     hashrate: 110,
-    powerConsumption: 23,
-    asicPrice: 2530,
+    powerConsumption: 2530,
+    asicPrice: 910,
     image: "./img/calculator/models/S19k Pro.jpg"
   },
   {
     label: "ANTMINER S19 SERIES",
     model: "S19j Pro+",
     hashrate: 117,
-    powerConsumption: 27.5,
-    asicPrice: 3218,
+    powerConsumption: 3218,
+    asicPrice: 1500,
     image: "./img/calculator/models/S19i Pro+.jpg"
   },
   {
     label: "ANTMINER S19 SERIES",
     model: "S19 Pro Hvd",
     hashrate: 126,
-    powerConsumption: 27.5,
-    asicPrice: 4650,
+    powerConsumption: 5428,
+    asicPrice: 1690,
     image: "./img/calculator/models/S19 Pro Hydro.jpg"
   },
   {
     label: "ANTMINER S19 SERIES",
     model: "S19 Pro+ Hvd",
     hashrate: 119,
-    powerConsumption: 27.5,
+    powerConsumption: 5253,
     asicPrice: 1770,
     image: "./img/calculator/models/S19 Pro+ Hydro.jpg"
   },
@@ -115,7 +115,7 @@ export const minerModels: MinerModel[] = [
     label: "ANTMINER S21 SERIES",
     model: "S21 Pro",
     hashrate: 234,
-    powerConsumption: 15.0,
+    powerConsumption: 3510,
     asicPrice: 4430,
     image: "./img/calculator/models/S21 Pro.jpg"
   },
@@ -123,23 +123,23 @@ export const minerModels: MinerModel[] = [
     label: "ANTMINER S21 SERIES",
     model: "S21 XP",
     hashrate: 207,
-    powerConsumption: 13.5,
-    asicPrice: 3850,
+    powerConsumption: 3645,
+    asicPrice: 6320,
     image: "./img/calculator/models/S21 XP.jpg"
   },
   {
     label: "ANTMINER S21 SERIES",
     model: "S21",
     hashrate: 188,
-    powerConsumption: 17.5,
-    asicPrice: 3290,
+    powerConsumption: 3290,
+    asicPrice: 3310,
     image: "./img/calculator/models/S21.jpg"
   },
   {
     label: "ANTMINER S21 SERIES",
     model: "T21",
     hashrate: 190,
-    powerConsumption: 19,
+    powerConsumption: 3610,
     asicPrice: 2920,
     image: "./img/calculator/models/T21.jpg"
   },
@@ -147,7 +147,7 @@ export const minerModels: MinerModel[] = [
     label: "ANTMINER S21 SERIES",
     model: "S21 Hvdro",
     hashrate: 260,
-    powerConsumption: 16,
+    powerConsumption: 5104,
     asicPrice: 5980,
     image: "./img/calculator/models/S21 Hydro.jpg"
   },
@@ -164,7 +164,7 @@ export const minerModels: MinerModel[] = [
     model: "L9",
     hashrate: 16,
     powerConsumption: 3360,
-    asicPrice: 7080,
+    asicPrice: 11180,
     image: "./img/calculator/models/L9.jpg"
   },
   {
@@ -179,16 +179,65 @@ export const minerModels: MinerModel[] = [
     label: "ANTMINER L7 SERIES",
     model: "L9",
     hashrate: 9500,
-    powerConsumption: 3450,
-    asicPrice: 5225,
+    powerConsumption: 3360,
+    asicPrice: 7080,
     image: "./img/calculator/models/L9.jpg"
   },
   {
     label: "ANTMINER L7 SERIES",
-    model: "L9",
-    hashrate: 9300,
+    model: "L7",
+    hashrate: 9500,
     powerConsumption: 3450,
+    asicPrice: 5225,
+    image: "./img/calculator/models/L9.jpg"
+  },
+	{
+    label: "ANTMINER L7 SERIES",
+    model: "L7",
+    hashrate: 9300,
+    powerConsumption: 3350,
     asicPrice: 5115,
+    image: "./img/calculator/models/L9.jpg"
+  },
+	{
+    label: "ANTMINER L7 SERIES",
+    model: "L7",
+    hashrate: 9050,
+    powerConsumption: 3200,
+    asicPrice: 4978,
+    image: "./img/calculator/models/L9.jpg"
+  },
+	{
+    label: "ANTMINER L7 SERIES",
+    model: "L7",
+    hashrate: 8800,
+    powerConsumption: 3200,
+    asicPrice: 4840,
+    image: "./img/calculator/models/L9.jpg"
+  },
+	{
+    label: "ANTMINER L7 SERIES",
+    model: "L7",
+    hashrate: 8550,
+    powerConsumption: 3200,
+    asicPrice: 4703,
+    image: "./img/calculator/models/L9.jpg"
+  },
+	{
+    label: "ANTMINER L7 SERIES",
+    model: "L7",
+    hashrate: 8300,
+    powerConsumption: 3200,
+    asicPrice: 4565,
+    image: "./img/calculator/models/L9.jpg"
+  },
+
+	{
+    label: "ANTMINER L7 SERIES",
+    model: "L7",
+    hashrate: 8050,
+    powerConsumption: 3200,
+    asicPrice: 4428,
     image: "./img/calculator/models/L9.jpg"
   },
   {
